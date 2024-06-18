85e44816d99d45199c78980ee41cbfa27a1ba450
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2024-06-18 10:51:22 -0500

    update to run

--- src/app/models/Url.ts
+++ src/app/models/Url.ts
@@ -1,5 +1,5 @@
 export class Url {
-    url = "https://energy-back.herokuapp.com";
+    url = "https://energy.clavedigitalhost.com";
     //url = "http://127.0.0.1:8000";
     public base = this.url+"/api/angular";
     public baseLogin = this.url+"/";
